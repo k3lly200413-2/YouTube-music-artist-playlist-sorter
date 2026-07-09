8ba5f7977c10f5c0315f4b0bd94a6c3718cc5e75
Kelly Applebee <kelly.applebee@studio.unibo.it>
2026-07-09 20:03:30 +0200

    feat: begin creation of main file

--- main.py
+++ main.py
@@ -1,27 +1,16 @@
-from ytmusicapi import YTMusic
-import time
 from Setup import setup
-from fileinput import FileInput
+from ytmusicapi import YTMusic
 import os
 
-# def check
-
 yt = YTMusic("browser.json")
-library_song_count = {}
+
+setup()
 playlists = yt.get_library_playlists(limit=None)
 
 
-if not os.path.isfile("firstSetup"):
-    setup()
-    with open("firstSetup", "w") as f:
-        for playlist in playlists:
-            f.write(f"{playlist['playlistId']} {playlist['trackCount']}\n")
+if not os.path.isfile("PrimoSetup"):
+    with open("PrimoSetup", "w") as f:
+        for p in playlists:
+            f.write(f"{p["playlistId"]} {p["count"]}\n")
 
-with open("firstSetup", "r") as f:
-    lines = [line.strip() for line in f]
 
-library_song_count = {}
-for entry in lines:
-    playlist_id, count = entry.rsplit(" ", 1)
-    library_song_count[playlist_id] = int(count)
-    
