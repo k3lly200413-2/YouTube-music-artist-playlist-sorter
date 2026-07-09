7a8dd8e046c1fedd1a06dca8b2edc32ffa823390
Kelly Applebee <kelly.applebee@studio.unibo.it>
2026-07-09 15:07:24 +0200

    feat: create a dictionary with the count of songs in each playlist

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 from ytmusicapi import YTMusic
 import time
-import Setup
+from Setup import setup
+from fileinput import FileInput
+import os
+
+# def check
+
+yt = YTMusic("browser.json")
+library_song_count = {}
+playlists = yt.get_library_playlists(limit=None)
+
+
+if not os.path.isfile("firstSetup"):
+    setup()
+    with open("firstSetup", "w") as f:
+        for playlist in playlists:
+            f.write(f"{playlist['playlistId']} {playlist['trackCount']}\n")
+
+with open("firstSetup", "r") as f:
+    lines = [line.strip() for line in f]
+
+library_song_count = {}
+for entry in lines:
+    playlist_id, count = entry.rsplit(" ", 1)
+    library_song_count[playlist_id] = int(count)
+    
