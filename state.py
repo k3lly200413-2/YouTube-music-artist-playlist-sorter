0f9c81e79e4a2affe22e7f3db9f3df93c34993d3
Kelly Applebee <kelly.applebee@studio.unibo.it>
2026-07-10 21:47:03 +0200

    feat: add read and write methods for json files to state

--- state.py
+++ state.py
@@ -1,3 +1,6 @@
+import json
+
+
 def get_playlist_id_from_name(playlist_name, playlists):
     return next((p["playlistId"] for p in playlists if p["title"] == playlist_name), None)
 
@@ -16,3 +19,11 @@ def update_dict():
         playlist_id, count = entry.rsplit(" ", 1)
         library_song_count[playlist_id] = int(count.replace(",", ""))
     return library_song_count
+
+def update_songs_json(playlist_data):
+    with open("playlists.json", "w") as f:
+        json.dump(playlist_data, f, indent=2)
+
+def read_songs_json():
+    with open("playlist.json") as f:
+        return json.load(f)
